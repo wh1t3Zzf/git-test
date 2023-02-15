808cdcf83e5560c8d3fefdfc1e1ecd176b69d2a9
Zife <1037015005@qq.com>
2023-02-15 14:00:56 +0800

    third commit

--- git-test/gittest/src/main/java/gittest/Test.java
+++ git-test/gittest/src/main/java/gittest/Test.java
@@ -2,8 +2,9 @@ package gittest;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("git-test2");
-		System.out.println("git-test2");
+		System.out.println("git-test3");
+		System.out.println("git-test3");
+		System.out.println("git-test3");
 	}
 	
 
